94ca88a5553877a67532778c8748a2722f73d90d
Nikhil Kohli <nikhilkohli2407@gmail.com>
2026-01-07 22:57:22 +0530

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://judix-coding-challenge.vercel.app/";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,

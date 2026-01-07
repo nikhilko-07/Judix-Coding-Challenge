65f87624f70ead7ec43d94efdc4452edef47dc46
Nikhil Kohli <nikhilkohli2407@gmail.com>
2026-01-07 23:28:26 +0530

    Change BASE_URL to localhost for development

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://judix-coding-challenge.vercel.app/";
+export const BASE_URL = "http://localhost:9000";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
